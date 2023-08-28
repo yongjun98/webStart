723f9244c7b15cb5085dc6f6a6ff5921f1768df4
yongjun98 <116868167+yongjun98@users.noreply.github.com>
2023-08-28 17:13:56 +0900

    간단한 게임 만들어보기

--- React-ToyProject/toyproject/src/App.js
+++ React-ToyProject/toyproject/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          리액트 프로젝트다!
+      
         </p>
         <a
           className="App-link"
